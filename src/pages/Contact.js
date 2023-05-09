fa82e455258d4e7c1b18aab4c580991f493e321d
Tomáš Gabriel <thomas2654cz@gmail.com>
2023-05-09 19:40:07 +0200

    Modified typography for h1

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,54 +7,54 @@ const Contact = () => {
       <CardHolder>
         <Card className="card-logo">
           <img
-            src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg"
-            alt="Facebook logo"
-          />
-        </Card>
-        <Card className="card-link-holder">
-          <p>
-            <a href="https://www.facebook.com/SomeoneCZ/">Facebook</a>
-          </p>
-        </Card>
-      </CardHolder>
-      <CardHolder>
-        <Card className="card-link-holder">
-          <p>
-            <a href="https://www.instagram.com/tomas_v_chill/">Instagram</a>
-          </p>
-        </Card>
-        <Card className="card-logo">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/9/95/Instagram_logo_2022.svg"
-            alt="Instagram logo"
-          />
-        </Card>
-      </CardHolder>
-      <CardHolder>
-        <Card className="card-logo">
-          <img
             src="https://upload.wikimedia.org/wikipedia/commons/7/7e/Gmail_icon_%282020%29.svg"
             alt="Gmail logo"
           />
         </Card>
         <Card className="card-link-holder">
-          <p>
+          <h1>
             <a href="mailto:thomas2654cz@gmail.com?subject=From%20virtual.xyz">
               Mail
             </a>
-          </p>
+          </h1>
         </Card>
       </CardHolder>
       <CardHolder>
         <Card className="card-link-holder">
-          <p>
+          <h1>
             <a href="https://github.com/VirtualCZ/">GitHub</a>
-          </p>
+          </h1>
         </Card>
         <Card className="card-logo">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
             alt="GitHub logo"
+          />
+        </Card>
+      </CardHolder>
+      <CardHolder>
+        <Card className="card-logo">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg"
+            alt="Facebook logo"
+          />
+        </Card>
+        <Card className="card-link-holder">
+          <h1>
+            <a href="https://www.facebook.com/SomeoneCZ/">Facebook</a>
+          </h1>
+        </Card>
+      </CardHolder>
+      <CardHolder>
+        <Card className="card-link-holder">
+          <h1>
+            <a href="https://www.instagram.com/tomas_v_chill/">Instagram</a>
+          </h1>
+        </Card>
+        <Card className="card-logo">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/9/95/Instagram_logo_2022.svg"
+            alt="Instagram logo"
           />
         </Card>
       </CardHolder>
