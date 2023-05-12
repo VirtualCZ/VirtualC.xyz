2a58da992438327925308bd8bd3dc2a931046384
Tomáš Gabriel <thomas2654cz@gmail.com>
2023-05-13 01:17:26 +0200

    Fixed issues with sidebar by making it openable from the side on mobile devices

--- src/elements/sidebar/Sidebar.js
+++ src/elements/sidebar/Sidebar.js
@@ -1,5 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import {
+  RiMenu2Line,
   RiHomeLine,
   RiSettingsLine,
   RiErrorWarningLine,
@@ -12,83 +13,121 @@ import './Sidebar.css';
 
 import DropdownButton from '../dropdownButton/DropdownButton';
 import { useTranslation } from 'react-i18next';
+import { useEffect, useState } from 'react';
 
 function Sidebar() {
   const { t, i18n } = useTranslation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMenuOpen(window.innerWidth > 992);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prevState) => !prevState);
+    console.log('swag');
+  };
 
   return (
-    <nav className='sidebar-container'>
-      <ul>
-        <li>
-          <NavLink exact to='/'>
-            <RiHomeLine /> <p>{t('navigation.home')}</p>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/React'>
-            <FaReact /> <p>React</p>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/ReactNative'>
-            <FaReact /> <p>React Native</p>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/Blender'>
-            <TbBrandBlender /> <p>Blender</p>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/niccalc'>
-            <ImLab /> <p>NicCalc</p>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/Contact'>
-            <RiInformationLine /> <p>{t('navigation.contact')}</p>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/404'>
-            <RiErrorWarningLine /> <p>404</p>
-          </NavLink>
-        </li>
-      </ul>
-      <ul className='sidebar-bottom'>
-        <li>
-          <div></div>
-          <DropdownButton
-            name={i18n.language}
-            options={[
-              {
-                option: 'Čeština',
-                onClick: () => {
-                  i18n.changeLanguage('cs');
+    <>
+      {!isMenuOpen && (
+        <a className='burger-menu' onClick={toggleMenu}>
+          <RiMenu2Line />
+          <p>{t('navigation.open')}</p>
+        </a>
+      )}
+
+      <nav className={`sidebar-container ${isMenuOpen ? 'open' : 'closed'}`}>
+        <ul>
+          {isMenuOpen && (
+            <li className='close-menu' onClick={toggleMenu}>
+              <a href='#'>
+                <RiMenu2Line />
+                <p>{t('navigation.close')}</p>
+              </a>
+            </li>
+          )}
+
+          <li>
+            <NavLink exact to='/'>
+              <RiHomeLine /> <p>{t('navigation.home')}</p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/React'>
+              <FaReact /> <p>React</p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/ReactNative'>
+              <FaReact /> <p>React Native</p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/Blender'>
+              <TbBrandBlender /> <p>Blender</p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/niccalc'>
+              <ImLab /> <p>NicCalc</p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/Contact'>
+              <RiInformationLine /> <p>{t('navigation.contact')}</p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/404'>
+              <RiErrorWarningLine /> <p>404</p>
+            </NavLink>
+          </li>
+        </ul>
+        <ul className='sidebar-bottom'>
+          <li>
+            <div></div>
+            <DropdownButton
+              name={i18n.language}
+              options={[
+                {
+                  option: 'Čeština',
+                  onClick: () => {
+                    i18n.changeLanguage('cs');
+                  },
                 },
-              },
-              {
-                option: 'English',
-                onClick: () => {
-                  i18n.changeLanguage('en');
+                {
+                  option: 'English',
+                  onClick: () => {
+                    i18n.changeLanguage('en');
+                  },
                 },
-              },
-              {
-                option: 'Deutsch',
-                onClick: () => {
-                  i18n.changeLanguage('de');
+                {
+                  option: 'Deutsch',
+                  onClick: () => {
+                    i18n.changeLanguage('de');
+                  },
                 },
-              },
-            ]}
-          />
-        </li>
-        <li>
-          <NavLink to='/settings'>
-            <RiSettingsLine /> <p>{t('navigation.settings')}</p>
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+              ]}
+            />
+          </li>
+          <li>
+            <NavLink to='/settings'>
+              <RiSettingsLine /> <p>{t('navigation.settings')}</p>
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 }
 
