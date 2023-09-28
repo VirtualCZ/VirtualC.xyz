80dbff83914f801648ea34721e14a95dcfa6ba01
Tomáš Gabriel <thomas2654cz@gmail.com>
2023-09-28 22:49:43 +0200

    Card.sass     cards are now 100% !important to fill the remaining space
    Projects.jsx
        basically completely rewritten
    
    Project.jsx
        has 2 components: ProjectDescription and ProjectImage

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -7,10 +7,32 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Stack from 'react-bootstrap/Stack';
+import { ProjectDescription, ProjectImage } from '../elements/navbar/Project';
 
-const Projects = ({ checked, toggleChecked }) => {
+const Projects = () => {
   const { t } = useTranslation();
   document.title = `${t('navigation.components')} - VirtualC`;
+  let webcount = 0
+  const webProjectsArr = [
+    {
+      name: "NicCalc",
+      text: "Introducing NicCalc, A Comprehensive E-liquid Mixing Calculator Built With JavaScript/ReactJS. NicCalc Empowers Users To Effortlessly Create Customized E-liquid Blends By Selecting The Desired VG/PG Ratios, Aroma Content Percentage, Ice Level And Nicotine Content With Additional Features Like Cost Calculation And Profit Margin Options. NicCalc Provides A Holistic Solution For Both Hobbyists And Professionals In The E-liquid Industry. Developed Using React Hooks And Custom CSS , This Intuitive And Versatile Calculate Offers A Seamless User Experience . Making E-liquid Mixing A Breeze",
+      icons: [{ image: "img", text: "text" }, { image: "img", text: "text" }, { image: "img", text: "text" }],
+      image: "swag",
+    },
+    {
+      name: "NicCalc",
+      text: "Introducing NicCalc, A Comprehensive E-liquid Mixing Calculator Built With JavaScript/ReactJS. NicCalc Empowers Users To Effortlessly Create Customized E-liquid Blends By Selecting The Desired VG/PG Ratios, Aroma Content Percentage, Ice Level And Nicotine Content With Additional Features Like Cost Calculation And Profit Margin Options. NicCalc Provides A Holistic Solution For Both Hobbyists And Professionals In The E-liquid Industry. Developed Using React Hooks And Custom CSS , This Intuitive And Versatile Calculate Offers A Seamless User Experience . Making E-liquid Mixing A Breeze",
+      icons: [{ image: "img", text: "text" }, { image: "img", text: "text" }, { image: "img", text: "text" }],
+      image: "swag",
+    },
+    {
+      name: "NicCalc",
+      text: "Introducing NicCalc, A Comprehensive E-liquid Mixing Calculator Built With JavaScript/ReactJS. NicCalc Empowers Users To Effortlessly Create Customized E-liquid Blends By Selecting The Desired VG/PG Ratios, Aroma Content Percentage, Ice Level And Nicotine Content With Additional Features Like Cost Calculation And Profit Margin Options. NicCalc Provides A Holistic Solution For Both Hobbyists And Professionals In The E-liquid Industry. Developed Using React Hooks And Custom CSS , This Intuitive And Versatile Calculate Offers A Seamless User Experience . Making E-liquid Mixing A Breeze",
+      icons: [{ image: "img", text: "text" }, { image: "img", text: "text" }, { image: "img", text: "text" }],
+      image: "swag",
+    },
+  ]
 
   return (
     <section
@@ -22,19 +44,27 @@ const Projects = ({ checked, toggleChecked }) => {
         <Card body style={{ maxWidth: 'fit-content' }}>
           <h2>Web projects</h2>
         </Card>
-        <Row>
-          <Col lg={12}>
-            <Card body >
-              <p>
-                I'm Tomáš Gabriel. I am from Czechia, and I studied IT at a high
-                school located in Uherské Hradiště called OAUH.
-              </p>
-              <p>
-                Here are some of my accounts, which you might find interesting:
-              </p>
-            </Card>
-          </Col>
-        </Row>
+
+        {webProjectsArr.map((w) => {
+          webcount++
+          if (webcount % 2) {
+            return (
+              <Row>
+                <ProjectDescription w={w} />
+                <ProjectImage w={w} />
+              </Row>
+            )
+          }
+          else {
+            return (
+              <Row>
+                <ProjectImage w={w} />
+                <ProjectDescription w={w} />
+              </Row>
+            )
+          }
+        })
+        }
       </Stack>
     </section>
   );
