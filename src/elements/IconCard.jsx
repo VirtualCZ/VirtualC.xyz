0947563bd4540a8a252491f97d704094b8c44b6a
Tomáš Gabriel <thomas2654cz@gmail.com>
2023-09-29 20:38:47 +0200

    IconCard - added rounded-pill

--- src/elements/IconCard.jsx
+++ src/elements/IconCard.jsx
@@ -3,7 +3,7 @@ import Image from "react-bootstrap/Image"
 
 const IconCard = ({ src, alt = "", text }) => {
     return (
-        <Card className="icon-card">
+        <Card className="icon-card rounded-pill">
             <Card.Body className="justify-content-center d-flex align-middle">
                 <Image draggable="false" fluid src={src} alt={alt} />
                 {text ?
