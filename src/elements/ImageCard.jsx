4b9ff8d08d83902f43dd0dab0c505e791268cb1e
Tomáš Gabriel <thomas2654cz@gmail.com>
2024-05-24 17:07:04 +0200

    fixed ImageCard issues for ios

--- src/elements/ImageCard.jsx
+++ src/elements/ImageCard.jsx
@@ -4,7 +4,7 @@ import Image from "react-bootstrap/Image"
 const ImageCard = ({ className = "", src, alt = "" }) => {
   return (
     <Card className={"image-card " + className} body>
-      <Image style={{ height: "100%", objectFit: "cover" }} draggable="false" fluid src={src} alt={alt} />
+      <Image className="object-fit-cover" draggable="false" fluid src={src} alt={alt} />
     </Card>
   )
 }
