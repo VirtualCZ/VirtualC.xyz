11930cd5f78ea4404ad79579e86ea6b15fb1c25d
Tomáš Gabriel <thomas2654cz@gmail.com>
2024-06-18 16:50:31 +0200

    Added Flex grow to ProjectDescription

--- src/elements/ProjectDescription.jsx
+++ src/elements/ProjectDescription.jsx
@@ -4,22 +4,24 @@ import IconCard from "./IconCard"
 const ProjectDescription = ({ w, className = "" }) => {
     return (
         <Col md={12} lg={7}>
-            <Card className={className} body>
-                <h2>
-                    {w.name}
-                </h2>
-                <p>
-                    {w.text}
-                </p>
-                <Row className='row-gap-3'>
-                    {w.icons.map((icon) => {
-                        return (
-                            <Col lg={12} xl={4} key={w.name + icon.text}>
-                                <IconCard link={icon.link} src={icon.image}>{icon.text}</IconCard>
-                            </Col>
-                        )
-                    })}
-                </Row>
+            <Card className={className} >
+                <Card.Body className="d-flex flex-column">
+                    <h2>
+                        {w.name}
+                    </h2>
+                    <p className="flex-grow-1">
+                        {w.text}
+                    </p>
+                    <Row className='row-gap-3'>
+                        {w.icons.map((icon) => {
+                            return (
+                                <Col lg={12} xl={4} key={w.name + icon.text}>
+                                    <IconCard link={icon.link} src={icon.image}>{icon.text}</IconCard>
+                                </Col>
+                            )
+                        })}
+                    </Row>
+                </Card.Body>
             </Card>
         </Col>
     )
