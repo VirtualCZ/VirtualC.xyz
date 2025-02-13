78e362fb03d9490818037367bb08b73c2755c38e
Tomáš Gabriel <tomas.gabriel@sitewell.cz>
2025-02-13 23:31:14 +0100

    made navbar usable when collapsed

--- src/elements/Navbar.jsx
+++ src/elements/Navbar.jsx
@@ -2,15 +2,14 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
 import { useTranslation } from 'react-i18next';
-import { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 
 function CustomNavbar() {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
 
   return (
     <Navbar
-      className='rounded-pill container navbar-fixed-top text-uppercase'
+      className='container navbar-fixed-top text-uppercase'
       variant='dark'
       expand='lg'
     >
