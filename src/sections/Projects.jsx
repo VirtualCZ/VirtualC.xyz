40a329f48f92c20371dc973e253eff84b6160570
Tomáš Gabriel <tomas.gabriel@sitewell.cz>
2025-02-13 20:44:54 +0100

    removed all mentions of Adobe XD

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -12,7 +12,6 @@ import TicketCalculator from "../assets/project-imgs/ticketcalculator.png"
 import oldVirtualCxyz from "../assets/project-imgs/oldvirtualcxyz.png"
 import figmaLogo from '../assets/logos/wh_figma_logo.svg';
 import ghLogo from '../assets/logos/wh_gh_logo.svg';
-import xdLogo from '../assets/logos/wh_xd_logo.svg';
 import globeLogo from '../assets/logos/wh_globe_logo.svg';
 import ImageCard from '../elements/ImageCard';
 
@@ -35,7 +34,6 @@ const Projects = ({ projectsSection }) => {
       text: "Explore WinUI playground, a dynamic web platform designed to showcase the functionality of various elements. Built using ReactJS with a custom Windows 11-inspired design, the site incorporates Bootstrap and Tailwind CSS for a responsive interface that adapts seamlessly across different devices. Navigation is enhanced with React Router for smooth transitions between pages. Hosted securely on Amazon Web Services (AWS), this project includes internationalization support through react-i18next and allows users to customize their experience with theme switching options.",
       icons: [
         { image: ghLogo, text: "GitHub", link: "https://github.com/VirtualCZ/VirtualC.xyz" },
-        { image: xdLogo, text: "Adobe XD" },
         { image: globeLogo, text: "Visit", link: "https://old.virtualc.xyz" }
       ],
       image: oldVirtualCxyz,
@@ -45,7 +43,6 @@ const Projects = ({ projectsSection }) => {
       text: "A ReactJS application built with DaisyUI for quick ticket price calculations. Add custom ticket types with prices in CZK and EUR, then calculate large ticket sales easily. The UI features a currency selector, fields for money received, number of tickets, type selection, total price, and change calculation. You can also clear inputs and manage ticket types (edit, delete, add).",
       icons: [
         { image: ghLogo, text: "GitHub", link: "https://github.com/VirtualCZ/TicketCalc" },
-        // { image: xdLogo, text: "Adobe XD" },
         { image: globeLogo, text: "Visit", link: "https://ticket.virtualc.xyz/" }
       ],
       image: TicketCalculator,
@@ -55,7 +52,6 @@ const Projects = ({ projectsSection }) => {
       text: "Discover NicCalc, a robust e-liquid mixing calculator crafted with JavaScript and ReactJS. This intuitive tool empowers users to create personalized e-liquid blends effortlessly. Customize VG/PG ratios, aroma percentages, ice levels, and nicotine content with ease. NicCalc also features cost estimation and profit margin settings, catering to hobbyists and industry professionals alike. Developed with React Hooks and custom CSS, NicCalc ensures a seamless user experience for streamlined e-liquid mixing.",
       icons: [
         { image: ghLogo, text: "GitHub", link: "https://github.com/VirtualCZ/VirtualC.xyz/tree/niccalc" },
-        { image: xdLogo, text: "Adobe XD" },
         { image: globeLogo, text: "Visit", link: "https://niccalc.virtualc.xyz/" }
       ],
       image: NicCalc,
